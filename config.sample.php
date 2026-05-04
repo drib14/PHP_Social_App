51354807a931c102fbb15429b4c0b2f954dd3322
drib14 <jhondribramirez7@gmail.com>
2026-05-04 19:33:55 +0800

    changes

--- config.sample.php
+++ config.sample.php
@@ -6,9 +6,9 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'socialize');
 
-define('CLOUDINARY_CLOUD_NAME', 'your_cloud_name');
-define('CLOUDINARY_API_KEY', 'your_api_key');
-define('CLOUDINARY_API_SECRET', 'your_api_secret');
+define('CLOUDINARY_CLOUD_NAME', 'dwquuisuj');
+define('CLOUDINARY_API_KEY', '655351295167741');
+define('CLOUDINARY_API_SECRET', 'F0UAKwbXYzDbcTbFr43iwL0D0qQ');
 
 // Base URL configuration (modify according to your setup)
 define('BASE_URL', 'http://localhost:8000');
